12b7a45332b4bc32b29234c8ca6f2e9820ac1ed4
mariopolo2805 <mariopolo2805@gmail.com>
2015-11-18 23:04:06 +0100

    Adding asides to mainMenu

--- public/mainMenu/controller.js
+++ public/mainMenu/controller.js
@@ -5,6 +5,23 @@ define([], function(){
         var vm = this;
 
         vm.name = "mainMenu";
+        vm.subjects = [
+            "SI1 - G301",
+            "SI1 - G302",
+            "PSI - G361",
+            "PSI - G362"
+        ];
+
+        vm.units = [
+            "Tema 1",
+            "Tema 2",
+            "Tema 3"
+        ];
+
+        vm.subject = function(name){
+            console.log(name);
+        };
+
     }
 
     return {
